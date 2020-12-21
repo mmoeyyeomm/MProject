ccbb8d10aa9f832557f77fa9f71bd030e968de77
mmoeyyeomm <75599758+mmoeyyeomm@users.noreply.github.com>
2020-12-21 11:23:08 +0700

    Add files via upload

--- Select menu.c
+++ Select menu.c
@@ -3,12 +3,14 @@
 int main(void){
 	int n = 0;
 	while (n != 2){
+		system("CLS");
 	printf("Select Menu");
 	printf("\n(1) Hello Janyamon");
 	printf("\n(2) END");
 	scanf("%d", &n);
 	if(n == 1){
 		printf("\nHello Janyamon\n");
+		getch();
 		}
 	}
 	return 0;
