821631744b9b0a00dfa4f4eec4a254f75edf280d
mmoeyyeomm <75599758+mmoeyyeomm@users.noreply.github.com>
2021-02-02 11:33:14 +0700

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+	int m, n, a, b, c, d;
+	float score, total, average;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	for(n = 1; n <= a, b, c, d; n++)
+	{
+			total = 0;
+			printf("student %d\n", n);
+			for(m = 1; m <= a + b + c + d; m++)
+	{
+		
+		scanf("%f", &score);
+		printf("%f", score);
+		total = total + score;
+	}		
+			average = total;
+			printf("\nThe average of student %d is %f n",n,average);
+	}
+			
+}
+
